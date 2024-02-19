0674c724adaf31ba524b22cf0802fda816abf801
Jack Martin <jackmartin.dev@gmail.com>
2024-02-19 22:43:09 +0900

    feat: Add mantine UI login form and navigation bar

---
+++ frontend/src/components/buttons/TwitterButton.tsx
@@ -0,0 +1,14 @@
+import { Button, ButtonProps } from '@mantine/core';
+import { TwitterIcon } from '@mantinex/dev-icons';
+
+const TwitterButton = (props: ButtonProps & React.ComponentPropsWithoutRef<'button'>) => {
+    return (
+        <Button
+            leftSection={<TwitterIcon style={{ width: '1rem', height: '1rem' }} color="#00ACEE" />}
+            variant="default"
+            {...props}
+        />
+    );
+}
+
+export default TwitterButton
