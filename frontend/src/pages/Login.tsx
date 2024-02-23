7a89f66569e4f294badc5eb17cb664a4cfb72d44
Jack Martin <jackmartin.dev@gmail.com>
2024-02-23 17:31:42 +0900

    feat: Add redirect to dashboard for logged in users

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,18 @@
+import { Navigate } from "react-router";
 import LoginForm from "../components/Login/LoginForm"
+import { useAuth } from "../context/AuthContext";
 
 const LoginPage = () => {
-    return(
-        <LoginForm/>
-    )
+    const {user} = useAuth();
+    console.log(user)
+
+    if (user) {
+        return <Navigate to="/dashboard"/>;
+    } else {
+        return (
+            <LoginForm />
+        );
+    }
 }
 
 export default LoginPage
