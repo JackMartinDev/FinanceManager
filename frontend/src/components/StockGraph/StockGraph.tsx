b640d8df7dbe33c1bb8aa50d54b197062613c9bc
Jack Martin <jackmartin.dev@gmail.com>
2024-02-26 18:06:29 +0900

    feat: Add styles for period buttons and active button state

--- frontend/src/components/StockGraph/StockGraph.tsx
+++ frontend/src/components/StockGraph/StockGraph.tsx
@@ -1,6 +1,9 @@
 import { CartesianGrid, Line, LineChart, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
 import testData from "./tempData.json"
-import { LineChart as MantineLineChart } from '@mantine/charts';
+import { useState } from "react";
+import { ActionIcon, Anchor, Box, Burger, Container, Group } from "@mantine/core";
+import classes from "./StockGraph.module.css"
+import { DoubleHeader } from "./test";
 
 const formatDateStringAxis = (dateString: string) => {
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short' };
@@ -15,8 +18,54 @@ const formatDateStringTooltip = (dateString: string) => {
 }
 
 const StockGraph = () => {
+    const currentDate = new Date();
+
+    const [active, setActive] = useState(3);
+
+    const peroidOptions = ["1M", "3M", "6M", "1Y"];
+
+    const mainItems = peroidOptions.map((item, index) => (
+        <Anchor<'a'>
+            href={item}
+            key={item}
+            className={classes.mainLink}
+            data-active={index === active || undefined}
+            onClick={(event) => {
+                event.preventDefault();
+                setActive(index);
+            }}
+        >
+            {item}
+        </Anchor>
+    ));
+
+    switch(active){
+        case 0:
+            currentDate.setMonth(currentDate.getMonth() -1);
+            break;
+        case 1:
+            currentDate.setMonth(currentDate.getMonth() -3);
+            break;
+        case 2:
+            currentDate.setMonth(currentDate.getMonth() -6);
+            break;
+        case 3:
+            currentDate.setMonth(currentDate.getMonth() -12);
+            break;
+        default:
+            currentDate.setMonth(currentDate.getMonth() -12);
+    }
+
+    const comparisonDate = currentDate;
+
     const parsedData = testData.map(data => ({date: formatDateStringTooltip(data.date), IVV: data.close}))
-    const closeValues = parsedData.map(item => item.IVV);
+
+    const filteredData = parsedData.filter(item => {
+        const itemDate = new Date(item.date);
+        return itemDate >= comparisonDate;
+    });
+
+    const closeValues = filteredData.map(item => item.IVV);
 
     const minClose = Math.min(...closeValues);
     const maxClose = Math.max(...closeValues);
@@ -24,6 +73,7 @@ const StockGraph = () => {
     const minDomain = Math.floor(minClose / 5) * 5;
     const maxDomain = Math.ceil(maxClose / 5) * 5;
 
+    //Have these values also determined by the selected peroid state. 
     const tickCount = (maxDomain - minDomain) / 5 + 1;
 
     const CustomTooltip: React.FC<TooltipProps<number, string>> = ({ active, payload, label }) => {
@@ -43,13 +93,23 @@ const StockGraph = () => {
     };
 
     return(
-        <LineChart width={800} height={300} margin={{right:20}} data={parsedData}>
-            <CartesianGrid opacity={0.3} vertical={false}/>
-            <XAxis dataKey="date" tickFormatter={formatDateStringAxis} interval={40} tick={{fontSize: 12, fill: "#868e96"}} />
-            <YAxis domain={[minDomain, maxDomain]} tickCount={tickCount} tick={{fontSize: 12, fill: "#868e96"}}/>
-            <Tooltip position={{y:10}} content={<CustomTooltip/>} cursor={{strokeDasharray: "3 3"}} isAnimationActive={false} />
-            <Line type="linear" dataKey="IVV" stroke="#228AE5" strokeWidth={2} dot={false} />
-        </LineChart>
+        <>
+            <Container className={classes.inner}>
+                <Box className={classes.links}>
+                    <Group gap={0} justify="flex-end" className={classes.mainLinks}>
+                        {mainItems}
+                    </Group>
+                </Box>
+            </Container>
+
+            <LineChart width={800} height={300} margin={{right:20}} data={filteredData}>
+                <CartesianGrid opacity={0.3} vertical={false}/>
+                <XAxis dataKey="date" tickFormatter={formatDateStringAxis} interval={40} tick={{fontSize: 12, fill: "#868e96"}} />
+                <YAxis domain={[minDomain, maxDomain]} tickCount={tickCount} tick={{fontSize: 12, fill: "#868e96"}}/>
+                <Tooltip position={{y:10}} content={<CustomTooltip/>} cursor={{strokeDasharray: "3 3"}} isAnimationActive={false} />
+                <Line type="linear" dataKey="IVV" stroke="#228AE5" strokeWidth={2} dot={false} />
+            </LineChart>
+        </>
     )
 }
 
