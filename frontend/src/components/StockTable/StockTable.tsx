ed089c11a6ca270711ce0f107dd0326c2f53ae4d
Jack Martin <jackmartin.dev@gmail.com>
2024-03-04 12:32:53 +0900

    feat: Add stock table

---
+++ frontend/src/components/StockTable/StockTable.tsx
@@ -0,0 +1,48 @@
+import { Container, Table, ColorSwatch } from "@mantine/core"
+
+const stockData = [
+    { code: "IVV", name: "S&P 500 ETF", avgPrice: 38.08, gainLoss: 2898.42, units: 201, price: 52.50, change: 0.23, value: 10550, color: "#009790"},
+    { code: "APPL", name: "Apple", avgPrice: 109, gainLoss: 1.42, units: 1, price: 111.50, change: 0.01, value: 101, color: "#999790"},
+    { code: null, name: "Total", avgPrice: null, gainLoss: 9000, units: null, price: null, change: 0.01, value: 101, color: "#228AE5" },
+];
+//Add bold for final row
+
+const StockTable = () => {
+
+    const rows = stockData.map((stock) => (
+        <Table.Tr key={stock.code}>
+            <Table.Td><ColorSwatch color={stock.color} size={15}/></Table.Td>
+            <Table.Td>{stock.code}</Table.Td>
+            <Table.Td>{stock.name}</Table.Td>
+            <Table.Td>{stock.avgPrice}</Table.Td>
+            <Table.Td>{stock.gainLoss}</Table.Td>
+            <Table.Td>{stock.units}</Table.Td>
+            <Table.Td>{stock.price}</Table.Td>
+            <Table.Td>{stock.change}</Table.Td>
+            <Table.Td>{stock.value}</Table.Td>
+        </Table.Tr>
+    ));
+
+    return (
+        <Container w={1200} mb={50}>
+            <Table>
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th></Table.Th>
+                        <Table.Th>Code</Table.Th>
+                        <Table.Th>Name</Table.Th>
+                        <Table.Th>Avg Price</Table.Th>
+                        <Table.Th>Profit/Loss</Table.Th>
+                        <Table.Th>Units</Table.Th>
+                        <Table.Th>Price</Table.Th>
+                        <Table.Th>Change</Table.Th>
+                        <Table.Th>Value</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>{rows}</Table.Tbody>
+            </Table>
+        </Container>
+    );
+}
+
+export default StockTable
--- frontend/src/pages/Investments.tsx
+++ frontend/src/pages/Investments.tsx
@@ -1,9 +1,11 @@
 import StockGraph from "../components/StockGraph/StockGraph"
+import StockTable from "../components/StockTable/StockTable"
 
 const InvestmentsPage = () => {
     return(
         <>
             Investments
+            <StockTable/>
             <StockGraph/>
         </>
     )
