bc1a2da252e39f89ed393d5eb275ab20e3f63877
Jack Martin <jackmartin.dev@gmail.com>
2024-03-09 16:14:34 +0900

    feat: Modify color picker to custom color swatch component

--- frontend/src/components/StockList/StockList.tsx
+++ frontend/src/components/StockList/StockList.tsx
@@ -1,12 +1,24 @@
-import { Button, ColorPicker, Group, NumberInput, Select, rem } from "@mantine/core"
+import { Button, CheckIcon, ColorSwatch, Group, NumberInput, Select, SimpleGrid, Text, rem } from "@mantine/core"
 import testData from "./stocklist.json"
 import { IconCurrencyDollar } from "@tabler/icons-react";
 import { useState } from "react";
 
 const StockList = () => {
     const filteredData = testData.map(stock => `${stock.Code}: ${stock.Name}`);
-    const [color, setColor] = useState('#23b9de');
     const icon = <IconCurrencyDollar style={{ width: rem(16), height: rem(16), color: "#121212" }}/>
+    const colors = ['#2e2e2e', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14'];
+    const [selectedColor, setSelectedColor] = useState('#23b9de');
+
+    //TODO: Add a transparent color swatch that can be used to select a custom color from a color picker
+    const swatches = colors.map(color => (
+        <ColorSwatch 
+            color={color} 
+            component="button" 
+            onClick={()=> setSelectedColor(color)}
+            style={{color: "#fff", cursor: "pointer"}}
+        >
+            {selectedColor === color && <CheckIcon style={{ width: rem(12), height: rem(12) }} />}
+        </ColorSwatch>))
 
     return (
         <>
@@ -24,14 +36,11 @@ const StockList = () => {
                 <NumberInput thousandSeparator="," hideControls decimalScale={2} flex={1} label="Price" leftSection={icon}/>
                 <NumberInput allowDecimal={false} flex={1} label="Amount"/>
             </Group>
+            <Text size="sm" style={{fontWeight: 500}}>Select Color</Text>
             <Group justify="space-between" align="end">
-                <ColorPicker
-                    value={color} 
-                    onChange={setColor}
-                    format="hex"
-                    swatches={['#2e2e2e', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']}
-                    withPicker={false}
-                    />
+                <SimpleGrid cols={7} spacing={4} verticalSpacing={4}>
+                    {swatches}
+                </SimpleGrid>
                 <Button >Add</Button>
             </Group>
         </>
