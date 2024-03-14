6f6a96856a2b0e0fcee70bfbf224ef7a7fadca5c
Jack Martin <jackmartin.dev@gmail.com>
2024-03-14 09:48:26 +0900

    chore: un-commented code and add tsx

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,7 +29,7 @@ app.use((req, _, next) => {
 app.use("/auth",authRoutes);
 
 //Prevent non logged in users from accessing content
-//app.use(checkAuthMiddleware);
+app.use(checkAuthMiddleware);
 
 app.use("/test", userRoutes);
 
