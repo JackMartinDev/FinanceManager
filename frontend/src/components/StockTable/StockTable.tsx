2a5b63203defefc3413f18ed57f6ab7a8ce6701d
Jack Martin <jackmartin.dev@gmail.com>
2024-03-18 17:31:06 +0900

    feat: Add total row and weight column to table

--- frontend/src/components/StockTable/StockTable.tsx
+++ frontend/src/components/StockTable/StockTable.tsx
@@ -13,52 +13,78 @@ type Props = {
     data: Stocks[]
 }
 const StockTable = ({data}: Props) => {
-    //Maybe remove the empty table clause since it isnt shown anyway
-    const rows = data.length > 0 ? data.map(({code, name, color, avgPrice, volume, data}) => {
+    let totalProfit = 0;
+    let totalChange = 0;
+
+    const totalValue = data.reduce((acc, { volume, data }) => {
+        const lastClose = data?.[data.length - 1]?.close ?? 0;
+        return acc + (lastClose * volume);
+    }, 0);
+
+    const rows = data.map(({code, name, color, avgPrice, volume, data}) => {
         const lastClose = data?.[data.length -1]?.close ?? 0;
         const previousClose = data?.[data.length -2]?.close ?? 0;
         const profit = (lastClose - avgPrice) * volume; 
         const change = lastClose - previousClose;
+        const changePercentage = (change) / previousClose * 100;
         const value = lastClose * volume;
-        
+        const weight = (value / totalValue) * 100;
+
+        totalProfit += profit;
+        totalChange += change * volume;
+
         return (
-        <Table.Tr key={code}>
-            <Table.Td><ColorSwatch color={color} size={15}/></Table.Td>
-            <Table.Td>{code}</Table.Td>
-            <Table.Td>{name}</Table.Td>
-            <Table.Td>{avgPrice}</Table.Td>
-            <Table.Td>{profit.toFixed(2)}</Table.Td>
-            <Table.Td>{volume}</Table.Td>
-            <Table.Td>{lastClose}</Table.Td>
-            <Table.Td>{change.toFixed(2)}</Table.Td>
-            <Table.Td>{value.toFixed(2)}</Table.Td>
+            <Table.Tr key={code}>
+                <Table.Td><ColorSwatch color={color} size={15}/></Table.Td>
+                <Table.Td>{code}</Table.Td>
+                <Table.Td>{name}</Table.Td>
+                <Table.Td>{avgPrice}</Table.Td>
+                <Table.Td>{profit.toFixed(2)}</Table.Td>
+                <Table.Td>{volume}</Table.Td>
+                <Table.Td>{lastClose}</Table.Td>
+                <Table.Td>{`${change.toFixed(2)}(${changePercentage.toFixed(2)}%)`}</Table.Td>
+                <Table.Td>{value.toFixed(2)}</Table.Td>
+                <Table.Td>{weight.toFixed(2)}%</Table.Td>
             </Table.Tr>
         );
-    }) : (
-            <Table.Tr>
-                <Table.Td colSpan={9} align="center">
-                    No stocks available.
-                </Table.Td>
-            </Table.Tr>
-        );
+    });
+
+    const totalRow = (
+        <Table.Tr>
+            <Table.Td></Table.Td>
+            <Table.Td></Table.Td>
+            <Table.Td>Total Holdings</Table.Td>
+            <Table.Td></Table.Td>
+            <Table.Td>{totalProfit.toFixed(2)}</Table.Td>
+            <Table.Td></Table.Td>
+            <Table.Td></Table.Td>
+            <Table.Td>{totalChange.toFixed(2)}</Table.Td>
+            <Table.Td>{totalValue.toFixed(2)}</Table.Td>
+            <Table.Td>100%</Table.Td>
+        </Table.Tr>
+    );
 
     return (
-            <Table>
-                <Table.Thead>
-                    <Table.Tr>
-                        <Table.Th></Table.Th>
-                        <Table.Th>Code</Table.Th>
-                        <Table.Th>Name</Table.Th>
-                        <Table.Th>Avg Price</Table.Th>
-                        <Table.Th>Profit/Loss</Table.Th>
-                        <Table.Th>Units</Table.Th>
-                        <Table.Th>Price</Table.Th>
-                        <Table.Th>Change</Table.Th>
-                        <Table.Th>Value</Table.Th>
-                    </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
+        <Table>
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th></Table.Th>
+                    <Table.Th>Code</Table.Th>
+                    <Table.Th>Name</Table.Th>
+                    <Table.Th>Avg Price</Table.Th>
+                    <Table.Th>Profit/Loss (%)</Table.Th>
+                    <Table.Th>Units</Table.Th>
+                    <Table.Th>Price</Table.Th>
+                    <Table.Th>Change (%)</Table.Th>
+                    <Table.Th>Value</Table.Th>
+                    <Table.Th>Weight</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+                {rows}
+                {totalRow}
+            </Table.Tbody>
+        </Table>
     );
 }
 
