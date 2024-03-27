4a4538b6daa4effb6d2994d7f81bb6480ecaa3bd
Jack Martin <jackmartin.dev@gmail.com>
2024-03-27 13:18:35 +0900

    feat: Add buttons and calendar to change the active year for the bar chart

--- frontend/src/pages/Subscriptions.tsx
+++ frontend/src/pages/Subscriptions.tsx
@@ -1,5 +1,5 @@
 import { Button, Group, Stack } from "@mantine/core"
-import { DateValue, MonthPickerInput } from '@mantine/dates';
+import { DateValue, MonthPickerInput, YearPickerInput } from '@mantine/dates';
 import SubscriptionsGraph from "../components/Subscriptions/Graph/SubscriptionsGraph"
 import SubscriptionList from "../components/Subscriptions/List/SubscriptionsList"
 import { useQuery } from "@tanstack/react-query"
@@ -19,6 +19,7 @@ import dayjs from "dayjs"
 const SubscriptionsPage = () => {
     const {user} = useAuth()
     const [activeMonth, setActiveMonth] = useState(dayjs().format("YYYY-MM"))
+    const [activeYear, setActiveYear] = useState(dayjs().format("YYYY"))
 
     const {data: subscriptionsData} = useQuery<SubscriptionList>({
         queryKey: ['subscriptions', user?.id],
@@ -30,18 +31,33 @@ const SubscriptionsPage = () => {
         (sub) => sub.month === activeMonth
     );
 
-    const prevDateHandler = () => {
+    const prevMonthHandler = () => {
         const previousMonth = dayjs(activeMonth).subtract(1, "month")
         setActiveMonth(previousMonth.format("YYYY-MM"))
     }
 
-    const nextDateHandler = () => {
-        const previousMonth = dayjs(activeMonth).add(1, "month")
-        setActiveMonth(previousMonth.format("YYYY-MM"))
+    const nextMonthHandler = () => {
+        const nextMonth = dayjs(activeMonth).add(1, "month")
+        setActiveMonth(nextMonth.format("YYYY-MM"))
     }
 
-    const changeDateHandler = (date: DateValue) => {
+    const prevYearHandler = () => {
+        const previousYear = dayjs(activeYear).subtract(1, "year")
+        setActiveYear(previousYear.format("YYYY"))
+    }
+
+    const nextYearHandler = () => {
+        const nextYear = dayjs(activeYear).add(1, "year")
+        setActiveYear(nextYear.format("YYYY"))
+    }
+
+
+    const changeMonthHandler = (date: DateValue) => {
         setActiveMonth(dayjs(date).format("YYYY-MM"))
+    }
+
+    const changeYearHandler = (date: DateValue) => {
+        setActiveYear(dayjs(date).format("YYYY"))
     }
 
     return(
@@ -50,19 +66,33 @@ const SubscriptionsPage = () => {
             <Group align="start">
                 <Stack>
                     <Group>
-                        <Button onClick={prevDateHandler}>Prev</Button>
+                        <Button onClick={prevMonthHandler}>Prev</Button>
                         <MonthPickerInput
                             placeholder="Pick date"
                             value={new Date(activeMonth)}
-                            onChange={(val) => {changeDateHandler(val)}}
+                            onChange={(val) => {changeMonthHandler(val)}}
+                            maxDate={new Date(dayjs().format("YYYY-MM"))}
                         />
-                        <Button onClick={nextDateHandler}>Next</Button>
+                        <Button onClick={nextMonthHandler} disabled={activeMonth === dayjs().format("YYYY-MM")}>Next</Button>
                     </Group>
-                        <SubscriptionList data={activeMonthSubscription}/>
+                    <SubscriptionList data={activeMonthSubscription}/>
                 </Stack>
-                {subscriptionsData &&
-                    <SubscriptionsGraph data={subscriptionsData.monthlyList}/>
-                }
+                <Stack>
+                    <Group>
+                        <Button onClick={prevYearHandler}>Prev</Button>
+                        <YearPickerInput
+                            placeholder="Pick date"
+                            value={new Date(activeYear)}
+                            onChange={(val) => {changeYearHandler(val)}}
+                            maxDate={new Date(dayjs().format("YYYY"))}
+                        />
+                        <Button onClick={nextYearHandler} disabled={activeYear === dayjs().format("YYYY")}>Next</Button>
+                    </Group>
+
+                    {subscriptionsData &&
+                        <SubscriptionsGraph data={subscriptionsData.monthlyList}/>
+                    }
+                </Stack>
             </Group>
         </>
     )
