76f55f2335b5c9b84c248a8df256bf6e63188432
Jack Martin <jackmartin.dev@gmail.com>
2024-07-19 16:47:46 +0900

    feat: Replace rsuit with fullcalendar

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -1,13 +1,22 @@
 import { Box, Title } from "@mantine/core"
-import { Calendar } from 'rsuite';
-import 'rsuite/Calendar/styles/index.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 const CalendarPage = () => {
     return(
         <Box w={1000}>
             <Title mb={16}>Calendar</Title>
             For when bills are due etc.
-            <Calendar bordered/>
+            <FullCalendar
+                plugins={[ dayGridPlugin, interactionPlugin ]}
+                initialView="dayGridMonth"
+                editable={true}
+                events={[
+                    { title: 'event 1', date: '2024-07-01' },
+                    { title: 'event 2', date: '2024-07-02' }
+                ]}
+            />
         </Box>
     )
 }
